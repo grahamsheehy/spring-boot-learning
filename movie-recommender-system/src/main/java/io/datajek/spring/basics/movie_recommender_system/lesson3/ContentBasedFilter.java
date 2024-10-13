540c3c80f718ec352325da3c677f62d376c9ac0c
Graham Sheehy <graham.sheehy@gmail.com>
2024-10-13 11:34:38 +0100

    Update ContentBasedFilter.java

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson3/ContentBasedFilter.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson3/ContentBasedFilter.java
@@ -1,7 +1,7 @@
 package io.datajek.spring.basics.movie_recommender_system.lesson3;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class ContentBasedFilter implements Filter {
 	public String[] getRecommendations(String movie ) {
 		//use content based filter to find similar movies
